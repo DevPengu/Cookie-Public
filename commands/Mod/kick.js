917357498aef56b6f18f10b29a9bc206ef42e05a
JustWolf <JustWolf@cloudradio.cz>
2019-09-21 15:13:41 +0200

    Update kick.js

--- commands/Mod/kick.js
+++ commands/Mod/kick.js
@@ -1,6 +1,15 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
+
+  let settings;
+  try {
+      settings = await client.getGuild(message.guild);
+  } catch (error) {
+      console.error(error);
+  }
+
+
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     let botRole = message.guild.members.get(client.user.id).highestRole;
