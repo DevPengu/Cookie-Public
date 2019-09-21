8e1215fc60ff529e6b3a4ae5ea1f20269b78f10b
vampymaria <38189579+vampymaria@users.noreply.github.com>
2019-09-21 08:38:20 -0400

    fix commands

--- commands/Mod/ban.js
+++ commands/Mod/ban.js
@@ -1,10 +1,21 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+  
+  let settings;
+  try {
+      settings = await client.getGuild(message.guild);
+  } catch (error) {
+      console.error(error);
+  }
+
+
+
+  let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.slice(1).join(" ") || "No reason"
-      if(!message.member.hasPermission("MANAGE_MEMBERS")) return errors.noPerms(message, "MANAGE_MEMBERS")
+      if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Nope! You can't do that!")
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
 
     let banEmbed = new Discord.RichEmbed()
@@ -16,9 +27,8 @@ module.exports.run = async (client, message, args) => {
     .addField("Time", message.createdAt)
     .addField("Reason", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "mod-log");
-    if(!incidentchannel) return message.channel.send("Can't find channel named 'mod-log'.");
-
+    let incidentchannel = message.guild.channels.find(settings.modLog)
+    if(!incidentchannel) return message.channel.send("Cannot find your modLog in my settings! Please set one!")
     message.guild.member(bUser).ban(bReason);
     incidentchannel.send(banEmbed);
 
